c7fa08f5a688d7857ffe6e12ce794d75f507fb12
Dhairyashil Bhosale <aryabhosale745@gmail.com>
2026-02-05 23:02:38 +0530

    added jwtutil and configurations

---
+++ music-streaming-backend/src/main/java/com/backend/dto/response/AuthResponse.java
@@ -0,0 +1,22 @@
+package com.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+ 
+    private String token;
+    
+
+    private String role;
+    
+
+    private Boolean premium;
+}
