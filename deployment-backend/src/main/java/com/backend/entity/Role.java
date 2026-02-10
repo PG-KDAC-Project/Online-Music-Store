acfd7542cd091497d91ac22d0f3095668449cb7e
Dhairyashil Bhosale <88606942+aryabhosale@users.noreply.github.com>
2026-02-11 02:45:07 +0530

    Refactor Role class

--- deployment-backend/src/main/java/com/backend/entity/Role.java
+++ deployment-backend/src/main/java/com/backend/entity/Role.java
@@ -1,38 +1,14 @@
 package com.backend.entity;
 
-import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
-@Entity
-@Table(name = "roles")
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+public enum Role implements GrantedAuthority {
+    ADMIN,
+    ARTIST,
+    LISTENER;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 20)
-    private ERole name;
-
-    public Role() {
-    }
-
-    public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
-
-    public void setName(ERole name) {
-        this.name = name;
+    @Override
+    public String getAuthority() {
+        return name();
     }
 }
